06e454c22bd0897dfef21a1771cce9907688686d
gloriahawkins <142853419+gloriahawkins@users.noreply.github.com>
2025-07-04 10:18:05 +1200

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,7 @@
 # data_loader.py
 import pandas as pd
 
+#basic function to load in the datasets
 def load_datasets(train_path, test_path):
     train_df = pd.read_csv(train_path)
     test_df = pd.read_csv(test_path)
